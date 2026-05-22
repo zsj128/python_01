63f152eb893edca676abacbb98da765b95a79738
zsj128 <3559985853@qq.com>
2026-05-22 15:25:12 +0800

    Add bookmanager.py

--- bookmanager.py
+++ bookmanager.py
@@ -7,8 +7,8 @@ class BookManager:
         try:
             self.conn = pymysql.connect(
                 host="localhost",
-                user="root",          # 改为自己的MySQL账号
-                password="Aa@1233211234567",# 改为自己的MySQL密码
+                user="root",          
+                password="Aa@1233211234567",
                 database="book_db",
                 charset="utf8mb4",
                 autocommit=False
@@ -70,6 +70,69 @@ class BookManager:
             print(f"状态：{res['state']}")
             print()
             self.write_log(f"用户{user_id}查询图书信息：图书编号{book_id}")
+        else:
+            print("❌ 未查询到该图书信息！")
+
+    def search_book_by_name(self, book_name,user_id):
+        sql = """
+        SELECT s.book_id, s.book_name, s.author, s.sort, s.state
+        FROM book s
+        WHERE s.book_name like %s
+        """
+        self.cursor.execute(sql, f'%{book_name}%')
+        res = self.cursor.fetchall()
+        if res:
+            for i in res:
+                print("\n========== 图书信息详情 ==========")
+                print(f"图书编号：{i['book_id']}")
+                print(f"书名：{i['book_name']}")
+                print(f"作者：{i['author']}")
+                print(f"分类：{i['sort']}")
+                print(f"状态：{i['state']}")
+                print()
+            self.write_log(f"用户{user_id}查询图书信息：图书名称为{book_name}")
+        else:
+            print("❌ 未查询到该图书信息！")
+
+    def search_book_by_sort(self, sort,user_id):
+        sql = """
+        SELECT s.book_id, s.book_name, s.author, s.sort, s.state
+        FROM book s
+        WHERE s.sort = %s
+        """
+        self.cursor.execute(sql, sort)
+        res = self.cursor.fetchall()
+        if res:
+            for i in res:
+                print("\n========== 图书信息详情 ==========")
+                print(f"图书编号：{i['book_id']}")
+                print(f"书名：{i['book_name']}")
+                print(f"作者：{i['author']}")
+                print(f"分类：{i['sort']}")
+                print(f"状态：{i['state']}")
+                print()
+            self.write_log(f"用户{user_id}查询图书信息：图书分类为{sort}")
+        else:
+            print("❌ 未查询到该图书信息！")
+
+    def search_book_by_author(self, author,user_id):
+        sql = """
+        SELECT s.book_id, s.book_name, s.author, s.sort, s.state
+        FROM book s
+        WHERE s.author = %s
+        """
+        self.cursor.execute(sql, author)
+        res = self.cursor.fetchall()
+        if res:
+            for i in res:
+                print("\n========== 图书信息详情 ==========")
+                print(f"图书编号：{i['book_id']}")
+                print(f"书名：{i['book_name']}")
+                print(f"作者：{i['author']}")
+                print(f"分类：{i['sort']}")
+                print(f"状态：{i['state']}")
+                print()
+            self.write_log(f"用户{user_id}查询图书信息：图书作者为{author}")
         else:
             print("❌ 未查询到该图书信息！")
 
@@ -147,6 +210,25 @@ class BookManager:
         else:
             print("❌ 未查询到该图书信息！")
         
+    def create_user(self,user_name,passwords):
+        sql = """
+        SELECT s.admin_id
+        FROM admin s
+        """
+        self.cursor.execute(sql)
+        res = self.cursor.fetchall()
+        for i in res:
+            if user_name==i['admin_id']:
+                print('用户已存在')
+                return
+        try:
+            sql = "INSERT INTO admin(admin_id,passwords) VALUES(%s,%s)"
+            self.cursor.execute(sql, (user_name,passwords))
+            self.conn.commit()
+            print("✅ 用户注册成功")
+        except Exception as e:
+            self.conn.rollback()
+            print(f"❌ 注册失败！用户id重复或数据格式错误")
 
     def is_users_id(self,users_id):
         sql="SELECT admin_id FROM admin where admin_id=%s"
@@ -173,6 +255,12 @@ class BookManager:
         self.conn.commit()
         print("✅ 修改密码成功")
 
+    def is_admin(self,user_id):
+        sql="SELECT is_admin FROM admin where admin_id=%s"
+        self.cursor.execute(sql,user_id)
+        self.conn.commit()
+        res = self.cursor.fetchall()
+        return res[0]['is_admin']
 
     # 关闭数据库连接
     def close(self):
@@ -184,82 +272,116 @@ class BookManager:
 def main():
     sm = BookManager()
     while True:
-        user_id=input("请输入管理员用户名（退出输入0）：")
-        if user_id=='0':
+        action=input("退出输入0|注册用户输入1|登入系统输入2：")
+        if action=='0':
             break
-        elif sm.is_users_id(user_id)==():
-            print('管理员用户不存在')
+        elif action=='1':
+            print("======= 用户注册 =======")
+            user_id=input("输入用户名：")
+            passwords=input("输入注册密码：")
+            sm.create_user(user_id,passwords)
             continue
-        else:
-            action=input("是否修改密码y/n：")
-            if action=='y':
-                passwords=input("请输入旧密码：")
+        elif action=='2':
+            print("======= 用户登入 =======")
+            user_id=input('输入用户名：')
+            if sm.is_users_id(user_id)==():
+                print('用户不存在')
+                continue
+            else:
+                action=input("是否修改密码y/n：")
+                if action=='y':
+                    passwords=input("请输入旧密码：")
+                    if not sm.is_passwords(user_id,passwords):
+                        continue
+                    else:
+                        new_password=input("请输入新密码：")
+                        sm.change_password(new_password,user_id)
+                passwords=input("请输入登入密码：")
                 if not sm.is_passwords(user_id,passwords):
                     continue
                 else:
-                    new_password=input("请输入新密码：")
-                    sm.change_password(new_password,user_id)
-            passwords=input("请输入登入密码：")
-            if not sm.is_passwords(user_id,passwords):
-                continue
-            else:
-                print("✅ 登入成功")
-                while True:
-                    print("\n======= 图书信息管理系统 =======")
-                    print("1. 添加图书信息")
-                    print("2. 查看所有图书完整信息")
-                    print("3. 按编号查询图书信息")
-                    print("4. 修改图书信息")
-                    print("5. 借阅图书")
-                    print("6. 归还图书")
-                    print("7. 删除图书信息")
-                    print("0. 退出系统")
-                    print("==========================================")
+                    is_admin_answer=sm.is_admin(user_id)
+                    print("✅ 登入成功")
+                    while True:
+                        print("\n======= 图书信息管理系统 =======")
+                        print("1. 添加图书信息（仅管理员）")
+                        print("2. 查看所有图书完整信息")
+                        print("3. 按编号查询图书信息")
+                        print("4. 按书名查询图书信息")
+                        print("5. 按分类查询图书信息")
+                        print("6. 按作者查询图书信息")
+                        print("7. 修改图书信息（仅管理员）")
+                        print("8. 借阅图书")
+                        print("9. 归还图书")
+                        print("10. 删除图书信息（仅管理员）")
+                        print("0. 退出系统")
+                        print("==========================================")
 
-                    choice = input("请输入功能编号：")
+                        choice = input("请输入功能编号：")
 
-                    if choice == "1":
-                        book_id = input("请输入图书编号：")
-                        book_name = input("请输入书名：")
-                        author = input("请输入作者姓名：")
-                        sort = input("请输入图书分类：")
-                        sm.add_book(book_id,book_name,author,sort,user_id)
+                        if choice == "1":
+                            if is_admin_answer=='是':
+                                book_id = input("请输入图书编号：")
+                                book_name = input("请输入书名：")
+                                author = input("请输入作者姓名：")
+                                sort = input("请输入图书分类：")
+                                sm.add_book(book_id,book_name,author,sort,user_id)
+                            else:
+                                print("❌ 无权限操作")
 
-                    elif choice == "2":
-                        sm.show_all_book()
+                        elif choice == "2":
+                            sm.show_all_book()
 
-                    elif choice == "3":
-                        book_id = input("请输入查询编号：")
-                        sm.search_book_by_id(book_id,user_id)
+                        elif choice == "3":
+                            book_id = input("请输入查询编号：")
+                            sm.search_book_by_id(book_id,user_id)
 
-                    elif choice == "4":
-                        book_id = input("请输入要修改的图书编号：")
-                        sm.search_book_by_id(book_id,user_id)
-                        book_name = input("请输入新的图书名称：")
-                        author = input("请输入新的作者名称：")
-                        sort = input("请输入新的图书分类：")
-                        sm.update_book_info(book_id, book_name, author, sort,user_id)
+                        elif choice == "4":
+                            book_name = input("请输入查询书名：")
+                            sm.search_book_by_name(book_name,user_id)
 
-                    elif choice == "5":
-                        book_name = input("请输入要借阅的图书名称：")
-                        sm.borrow_book(book_name,user_id)
-                    
-                    elif choice == "6":
-                        book_name = input("请输入要归还的图书名称：")
-                        sm.return_book(book_name,user_id)
+                        elif choice == "5":
+                            sort = input("请输入查询图书分类：")
+                            sm.search_book_by_sort(sort,user_id)
 
-                    elif choice == "7":
-                        book_id = input("请输入要删除的图书id：")
-                        action=input("确认是否删除该图书信息y/n：")
-                        if action=='y':
-                            sm.delete_book(book_id,user_id)
-                    elif choice == "0":
-                        sm.close()
-                        print("👋 系统退出成功，再见！")
-                        break
+                        elif choice == "6":
+                            author = input("请输入查询作者：")
+                            sm.search_book_by_author(author,user_id)
 
-                    else:
-                        print("❌ 输入无效，请输入0-6的数字！")
+                        elif choice == "7":
+                            if is_admin_answer=='是':
+                                book_id = input("请输入要修改的图书编号：")
+                                sm.search_book_by_id(book_id,user_id)
+                                book_name = input("请输入新的图书名称：")
+                                author = input("请输入新的作者名称：")
+                                sort = input("请输入新的图书分类：")
+                                sm.update_book_info(book_id, book_name, author, sort,user_id)
+                            else:
+                                print("❌ 无权限操作")
+
+                        elif choice == "8":
+                            book_name = input("请输入要借阅的图书名称：")
+                            sm.borrow_book(book_name,user_id)
+                        
+                        elif choice == "9":
+                            book_name = input("请输入要归还的图书名称：")
+                            sm.return_book(book_name,user_id)
+
+                        elif choice == "10":
+                            if is_admin_answer=='是':
+                                book_id = input("请输入要删除的图书id：")
+                                action=input("确认是否删除该图书信息y/n：")
+                                if action=='y':
+                                    sm.delete_book(book_id,user_id)
+                            else:
+                                print("❌ 无权限操作")
+                        elif choice == "0":
+                            sm.close()
+                            print("👋 系统退出成功，再见！")
+                            break
+
+                        else:
+                            print("❌ 输入无效，请输入0-6的数字！")
         break
 
 if __name__ == "__main__":
